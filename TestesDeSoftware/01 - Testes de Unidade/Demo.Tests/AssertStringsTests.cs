64d9ee7bff43b3ad99a7e43dfb7ec4713506f3c3
Carlos Aurelio <carlosspok@gmail.com>
2019-09-28 16:03:16 -0300

    Criando o projeto para Estudo de Testes de Software

---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/AssertStringsTests.cs
@@ -0,0 +1,85 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.Equal("Carlos Aurélio", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.Equal("CARLOS AURÉLIO", nomeCompleto, true);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.Contains("los Au", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.StartsWith("Car", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveTerminarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.EndsWith("lio", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Carlos", "Aurélio");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo/Funcionario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class Pessoa
+    {
+        public string Nome { get; protected set; }
+        public string Apelido { get; set; }
+    }
+
+    public class Funcionario : Pessoa
+    {
+        public double Salario { get; private set; }
+        public EnumNivelProfissional NivelProfissional { get; private set; }
+        public IList<string> Habilidades { get; private set; }
+
+        public Funcionario(string nome, double salario)
+        {
+            Nome = string.IsNullOrEmpty(nome) ? "Fulano" : nome;
+
+            DefinirSalario(salario);
+            DefinirHabilidades();
+        }
+
+        private void DefinirSalario(double salario)
+        {
+            if (salario < 500) throw new Exception("Salario inferior ao permitido.");
+
+            Salario = salario;
+
+            if (salario < 2000) NivelProfissional = EnumNivelProfissional.Junior;
+            else if (salario >= 2000 && salario < 8000) NivelProfissional = EnumNivelProfissional.Pleno;
+            else if (salario >= 8000) NivelProfissional = EnumNivelProfissional.Senior;
+        }
+
+        private void DefinirHabilidades()
+        {
+            var habilidadesBasicas = new List<string>()
+            {
+                "Logica de Programação",
+                "OOP"
+            };
+
+            Habilidades = habilidadesBasicas;
+
+            switch (NivelProfissional)
+            {
+                case EnumNivelProfissional.Pleno:
+                    Habilidades.Add("Testes");
+                    break;
+                case EnumNivelProfissional.Senior:
+                    Habilidades.Add("Testes");
+                    Habilidades.Add("Microservices");
+                    break;
+            }
+        }
+
+        public enum EnumNivelProfissional
+        {
+            Junior,
+            Pleno,
+            Senior
+        }
+
+        public class FuncionarioFactory
+        {
+            public static Funcionario Criar(string nome, double salario) => new Funcionario(nome, salario);
+        }
+    }
+}
