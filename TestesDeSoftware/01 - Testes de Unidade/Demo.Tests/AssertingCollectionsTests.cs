82a33d231bcf52b5a60b941577fb0827610f9a0e
Carlos Aurelio <carlosspok@gmail.com>
2019-09-28 16:24:54 -0300

    Adicionando Testes de Collections

---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/AssertingCollectionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingCollectionsTests
+    {
+        [Fact]
+        public void Funcionario_Habilidades_NaoDevePossuirHabilidadesVazias()
+        {
+            // Assert & Act
+            var funcionario = FuncionarioFactory.Criar("Carlos Aurélio", 10000);
+
+            // Assert
+            Assert.All(funcionario.Habilidades, habilidade => Assert.False(string.IsNullOrEmpty(habilidade)));
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorDevePossuirHabilidadeBasica()
+        {
+            // Assert & Act
+            var funcionario = FuncionarioFactory.Criar("Carlos Aurélio", 1000);
+
+            // Assert
+            Assert.Contains("OOP", funcionario.Habilidades);
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorNaoDevePossuirHabilidadeAvancada()
+        {
+            // Assert & Act
+            var funcionario = FuncionarioFactory.Criar("Carlos Aurélio", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", funcionario.Habilidades);
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_SeniorDevePossuirTodasHabilidades()
+        {
+            // Assert & Act
+            var funcionario = FuncionarioFactory.Criar("Carlos Aurélio", 10000);
+
+            var habilidadesBasicas = new[]
+            {
+                "Logica de Programação",
+                "OOP",
+                "Testes",
+                "Microservices"
+            };
+
+            // Assert
+            Assert.Equal(habilidadesBasicas, funcionario.Habilidades);
+        }
+    }
+}
