fdde501dc543c39cbb2d1d9b140b5db0fbcd8122
Carlos Aurelio <carlosspok@gmail.com>
2019-09-30 20:23:23 -0300

    Implementando fixtures

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,42 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    {
+    }
+
+    public class ClienteTestsFixture : IDisposable
+    { 
+        public Cliente GerarClienteValido()
+        {
+            return new Cliente(
+                Guid.NewGuid(),
+                "Carlos",
+                "Aurélio",
+                DateTime.Now.AddYears(-25),
+                "carlosspok@gmail.com",
+                true,
+                DateTime.Now);
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            return new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "carlosspok1gmail.com",
+                true,
+                DateTime.Now);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
